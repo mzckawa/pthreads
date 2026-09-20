7162bad593d0714b585332c54f0f85277b863054
mzckawa <5689.melzion.cmr@gmail.com>
2026-09-20 17:03:01 -0300

    Subindo a questao 4. O próprio código já realiza os testes com 1, 2 e 4 núcleos e mostra, ao final, os resultados dos tempos de execucao. Para sistemas pequenos (tipo 50x50), é comum acontecer de os melhores tempos serem com 2, 4 e 1 núcleos, respectivamente, entretanto, para sistemas grandes (400x400, como definido nessa versao final), o quarteto de núcleos toma o primeiro lugar e, quanto maior o sistema, maior a vantagem de desempenho desse nível de paralelizacao.

--- q4/q4.c
+++ q4/q4.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
-#define NUCLEOS_TESTE 1 // para fazer o teste com 1, 2, 4 núcleos
-#define TAM_SISTEMA 5// matriz será NxN, com N = TAM_SISTEMA
-#define P 10 // número de iterações
+#define TAM_SISTEMA 400// matriz será NxN, com N = TAM_SISTEMA
+#define P 100 // número de iterações
 
 // dados do sistema (compartilhados entre as threads)
 // usamos double para aumentar a precisão do método
@@ -15,6 +15,8 @@ double x_k[TAM_SISTEMA]; // vetor xˆ(k)
 double x_kmais1[TAM_SISTEMA]; // vetor xˆ(k + 1)
 
 pthread_barrier_t barreira;
+
+struct timespec inicio, fim;
 
 // struct com os dados que cada thread irá processar
 
@@ -63,14 +65,14 @@ void *calculo_jacobi(void *arg){
     pthread_exit(NULL);
 }
 
-void criar_threads(pthread_t *threads, DadosThread *dados, void *(*calculo_jacobi)(void *)){
+void criar_threads(pthread_t *threads, DadosThread *dados, void *(*calculo_jacobi)(void *), int nucleos_teste){
 
-    int tam_bloco = TAM_SISTEMA / NUCLEOS_TESTE;
-    int resto = TAM_SISTEMA % NUCLEOS_TESTE;
+    int tam_bloco = TAM_SISTEMA / nucleos_teste;
+    int resto = TAM_SISTEMA % nucleos_teste;
     int inicio_atual = 0;
 
     // distribuindo variáveis entre as threads
-    for (int i = 0; i < NUCLEOS_TESTE; i++){
+    for (int i = 0; i < nucleos_teste; i++){
 
         dados[i].id = i;
         dados[i].inicio = inicio_atual;
@@ -90,7 +92,76 @@ void criar_threads(pthread_t *threads, DadosThread *dados, void *(*calculo_jacob
     }
 }
 
-int main{
+int main(int argc, char *argv[]){
+
+    int testes[] = {1, 2, 4};
+    int total_testes = 3;
+
+    // inicializando x_k, colocando todos os valores iguais a 1
+    for(int k = 0; k < total_testes; k++){ // a cada iteração, é feito um teste
+
+        int nucleos_teste = testes[k];
+
+        pthread_t threads[nucleos_teste];
+        DadosThread dados[nucleos_teste]; 
+
+        for (int i = 0; i < TAM_SISTEMA; i ++){
+
+        x_k[i] = 1.0;
+        b[i] = 298.0; // vetor arbitrário de termos independentes
+
+            /*
+            O método de Jacobi funciona melhor quando A[i][i] 
+            tem valor absoluto maior do que a soma dos valores absolutos da linha i,
+            ou seja, quando a matriz é classificada como "estritamente diagonal dominante"
+            sabendo disso, para ter resultados mais claros, vamos testar em matrizes desse tipo.
+            */
+
+            for(int j = 0; j< TAM_SISTEMA; j++){
+
+                if(i == j){
+                    A[i][j] = 100.0; // para a diagonal principal ser bem maior do que o resto
+                }
+
+                else{
+                    A[i][j] = 1.0;
+                }
+            }
+        }
+    
+
+    // inicializando a barreira
+    pthread_barrier_init(&barreira, NULL, nucleos_teste);
+    
+    // registrando o tempo logo antes da criação das threads
+    clock_gettime(CLOCK_MONOTONIC, &inicio);
+
+    // disparando as threads
+    criar_threads(threads, dados, calculo_jacobi, nucleos_teste);
+
+    // fazendo as threads aguardarem a execução das P iterações
+    
+    for (int i = 0; i< nucleos_teste; i++){
+        pthread_join(threads[i], NULL);
+    }
+    
+    // registrando o tempo de término 
+    clock_gettime(CLOCK_MONOTONIC, &fim);
+
+    // após o encerramento das threads, podemos destruir as barreiras
+    pthread_barrier_destroy(&barreira);
+
+    // calculando o tempo de execução (cálculo de segundos inteiros, cálculo da diferença em nanossegundos, soma de ambas as partes de divisão por 10ˆ9 (1e9), para obter uma fração decimal de segundos precisa)
+    double tempo = (fim.tv_sec - inicio.tv_sec) + (fim.tv_nsec - inicio.tv_nsec) / 1e9;
+
+    // exibindo a solução final e o tempo de execução para a atual quantidade de núcleos
+    printf("Solução final após %d iterações:\n", P);
+    for (int i = 0; i < TAM_SISTEMA; i++){
+        printf("x[%d] = %.2f\n", i, x_k[i]);
+    }
+    printf("Execução final com %d núcleos: %.6f segundos.\n", nucleos_teste, tempo);
+
+    }
 
     return 0;
 }
